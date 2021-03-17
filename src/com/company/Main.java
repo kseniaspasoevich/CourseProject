f058789c995025ee52c76c1941b4699197b83982
spasoevich.ksenia <kseniya.spasoyevich@gmail.com>
2021-03-18 02:34:04 +0300

    Deleted extra code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,7 @@ import com.google.gson.JsonElement;
 
 public class Main {
     public static void main(String[] args) {
-
-        //Gson GSON = new Gson();
+        
         TimeTable obj = new TimeTable();
         obj.generateTimeScedule(10);
 
